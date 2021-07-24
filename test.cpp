a38a7c076d202d3a49141924fb971b4d51fe1a87
志诚 李 <1404204769@qq.com>
2021-07-24 14:09:47 +0800

    测试了随机数据的功能

--- test.cpp
+++ test.cpp
@@ -1,9 +1,27 @@
 #include<iostream>
+#include "data.h"
+#include "Cuser.h"
 using namespace std;
 int main() {
-	char str[10] = "李四";
-	char arr[10] = "";
-	cin >> arr;
-	cout << str << " ： " << arr << endl;
+	//char str[10] = "李四";
+	//char arr[10] = "";
+	//cin >> arr;
+	//cout << str << " ： " << arr << endl;
+	clock_t start, finish; //声明start和finish是两个时间
+	double time; //定义运行时间
+	start = clock(); //获取开始时间
+	cout << "随机生成100人的数据" << endl;
+	Cuser temUser;
+	for (int i = 0; i < 100; i++) {
+		showRandomName(temUser);
+		showRandomTel(temUser);
+		showRandomAddress(temUser);
+		cout <<i<< ".\tname:" << temUser.getName() << "\tTel:" << temUser.getTel() << "\tAddress:" << temUser.getAddress() << endl;
+	}
+	finish = clock();
+	printf("\n");
+	//time = (double)(finish - start);
+	time = (double)(finish - start) / CLOCKS_PER_SEC;
+	printf("RunningTime:\n%f 秒\n", time);//显示
 	return 0;
 }
